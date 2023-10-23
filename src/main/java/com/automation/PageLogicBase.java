3d584e18f3cd5404fa1fe309df11199b6b13bd68
noyku <noykush5@gmail.com>
2023-10-23 23:00:16 +0300

    first sanity tests

--- src/main/java/com/automation/PageLogicBase.java
+++ src/main/java/com/automation/PageLogicBase.java
@@ -1,6 +1,5 @@
 package com.automation;
 
-import com.automation.ElementVerify;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,32 +8,19 @@ import org.slf4j.LoggerFactory;
 public abstract class PageLogicBase {
     protected WebDriver driver;
     private DriverManager driverManager;
-    public ElementVerify verifyElement;
-    public PageLogicBase(String data_props_path, Long wait_sec,String logger_name){
-        driverManager = DriverManager.getInstance(data_props_path, wait_sec);
+    public ElementVerify elementVerify;
+    public SeleniumUtils seleniumUtils;
+    public PageLogicBase(String data_props_path){
+        driverManager = DriverManager.getInstance(data_props_path);
         driver = driverManager.getDriver();
-        verifyElement = new ElementVerify(driver, driverManager.getWait());
+        elementVerify = new ElementVerify(driverManager);
+        seleniumUtils = new SeleniumUtils(driverManager);
     }
 
-    public abstract void navigate();
+    // common actions for all web pages
+    public abstract void navigate(Boolean byUrl);
 
-
-
-    /*  Selenium wrapper methods  */
-    protected void clickElement(WebElement e){
-        try{
-            driverManager.getWait().until(ExpectedConditions.visibilityOf(e));
-            e.click();
-            LoggerFactory.getLogger("clickElement").debug("SUCCESS");
-        }catch (Exception exception) {
-            LoggerFactory.getLogger("clickElement").debug("FAILURE \n"+exception.getMessage());
-        }
+    public DriverManager getDriverManager() {
+        return driverManager;
     }
-    public void closeDriver(){
-        driver.close();
-    }
-    public void navigate_to(String url){
-        driver.get(url);
-    }
-
 }
